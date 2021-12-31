7da85b7224fea745051111c3cd60ed55e763a145
OmarRodr <73503629+OmarRodr@users.noreply.github.com>
2021-12-30 22:11:16 -0600

    Delete EvaluacionAnonima directory

--- EvaluacionAnonima/src/main/java/mx/edu/uacm/progweb/evaluacionanonima/service/UsuarioService.java
+++
@@ -1,7 +0,0 @@
-package mx.edu.uacm.progweb.evaluacionanonima.service;
-
-import mx.edu.uacm.progweb.evaluacionanonima.dominio.Usuario;
-
-public interface UsuarioService {
-	Usuario obtenerUsuarioPorCorreoYContrasenia(String correo, String contrasenia);
-}
